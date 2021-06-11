b89a71794880cfe0b4be4bc2f1e4dabb9d230b5e
Ricardo Delfin <me@rdelfin.com>
2021-06-11 22:23:19 +0100

    Added basic amethyst example

---
+++ src/state.rs
@@ -0,0 +1,55 @@
+use amethyst::{
+    assets::ProgressCounter,
+    core::transform::Transform,
+    input::{is_close_requested, is_key_down, VirtualKeyCode},
+    prelude::{Builder, World, WorldExt},
+    renderer::camera::Camera,
+    window::ScreenDimensions,
+    GameData, SimpleState, SimpleTrans, StateData, StateEvent, Trans,
+};
+
+#[derive(Default)]
+pub struct Game {
+    pub progress_counter: Option<ProgressCounter>,
+    pub items_done_last: Option<usize>,
+}
+
+impl SimpleState for Game {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let StateData { mut world, .. } = data;
+        // Crates new progress counter
+        self.progress_counter = Some(Default::default());
+
+        // Creates a new camera
+        initialise_camera(&mut world);
+    }
+
+    fn handle_event(
+        &mut self,
+        mut _data: StateData<'_, GameData<'_, '_>>,
+        event: StateEvent,
+    ) -> SimpleTrans {
+        if let StateEvent::Window(event) = &event {
+            if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                return Trans::Quit;
+            }
+        }
+        Trans::None
+    }
+}
+
+fn initialise_camera(world: &mut World) {
+    let (width, height) = {
+        let dim = world.read_resource::<ScreenDimensions>();
+        (dim.width(), dim.height())
+    };
+
+    let mut camera_transform = Transform::default();
+    camera_transform.set_translation_z(1.0);
+
+    world
+        .create_entity()
+        .with(camera_transform)
+        .with(Camera::standard_2d(width, height))
+        .build();
+}
